0dec1c9d86ab6b164e226e73aee388c2c1d0cf21
Milthon Hidalgo <milthon.hidalgo@gmail.com>
2022-12-18 05:23:09 -0300

    Primer commit

---
+++ postcss.config.js
@@ -0,0 +1,12 @@
+const purgecss = require('@fullhuman/postcss-purgecss');
+
+module.exports = {
+  plugins: [
+    require('postcss-import'),
+    require('autoprefixer'),
+    // require('cssnano')({ preset: 'default' }),
+    purgecss({
+      content: ['./**/*.html'],
+    }),
+  ],
+};
---
+++ scripts.js
@@ -0,0 +1,24 @@
+const form = document.getElementById('form');
+const email = document.getElementById('email');
+const message = document.getElementById('message');
+const errorIcon = document.getElementById('error-icon');
+
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+
+  if (validateEmail(email.value)) {
+    errorIcon.style.display = 'none';
+    message.style.display = 'none';
+  } else {
+    errorIcon.style.display = 'block';
+    message.style.display = 'block';
+  }
+});
+
+const validateEmail = (email) => {
+  const emailRegex =
+    /^(([^<>()\[\]\\.,:\s@"]+(\.[^<>()\[\]\\.,:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+
+  if (emailRegex.test(email)) return true;
+  else return false;
+};
